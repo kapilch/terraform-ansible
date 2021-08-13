99588ace43560ed490e3b51dfbee4a9de189e67b
kapil chawla <kc3020@gmail.com>
2021-08-13 22:52:04 +0100

    fixed ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,8 @@
+locals {
+  ssh_user = "ubuntu"
+  key_name = "SSHKeys"
+  private_key_path = "~/.ssh/SSHKeys.pem"
+}
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
@@ -6,16 +11,13 @@ provider "aws" {
 
 resource "aws_instance" "myFirstEC2Instance" {
   ami = "ami-0443305dabd4be2bc"
-  #ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  #subnet_id = aws_subnet.myFirstSubnet.id
 
   tags = {
     Name = "myFirstEC2Instance"
   }
-  # count = 2
 
-  provisinor "remote-exec" {
+  provisioner "remote-exec" {
     inline = ["echo 'wait until ssh is ready'"]
 
     connection {
@@ -26,7 +28,7 @@ resource "aws_instance" "myFirstEC2Instance" {
     }
   }
 
-  provisinor "local-exec" {
-    command = "ansible"
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.myFirstEC2Instance.public_ip}, --private-key ${local.private_key_path} nginx.yml"
   }
 }
