d409049787c1841445e162dd1e8da63d76f827ec
kapil chawla <kc3020@gmail.com>
2021-08-14 00:08:44 +0100

    fixed ec2.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "access_key" {
-  description = "aws key"
-  type        = string
-}
+# variable "access_key" {
+#   description = "aws key"
+#   type        = string
+# }
 
-variable "secret_key" {
-  description = "aws key"
-  type        = string
-}
+# variable "secret_key" {
+#   description = "aws key"
+#   type        = string
+# }
